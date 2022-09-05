a45ede8b7ff81483ff227f5e32c2d1180d45f179
Jimena Miguez <jimeblau@gmail.com>
2022-09-05 13:09:10 +0200

    Finish adding async data to a single post

--- pages/posts/_id/index.vue
+++ pages/posts/_id/index.vue
@@ -1,18 +1,54 @@
 <template>
   <div class="single-post-page">
     <section class="post">
-      <h1 class="post-title">Title of the Post</h1>
+      <!-- 3) Output the data of the fetch object with
+      the information of a single post. -->
+      <h1 class="post-title">{{ loadedPost.title }}</h1>
       <div class="post-details">
-        <div class="post-detail">Last updated on XXX</div>
-        <div class="post-detail">Written by NAME</div>
+        <div class="post-detail">Last updated on {{ loadedPost.updatedDate }}</div>
+        <div class="post-detail">Written by {{ loadedPost.author }}</div>
       </div>
-      <p class="post-content">Content of the post</p>
+      <p class="post-content">{{ loadedPost.content }}</p>
     </section>
     <section class="post-feedback">
       <p>Let me know what you think about the post, send a mail to <a href="mailto:feedback@my-awesome-domain.com">feedback@my-awesome-domain.com</a>.</p>
     </section>
+    <!-- 4) Now if I click on a single post in the posts
+    section I will be able to see the correct id. -->
   </div>
 </template>
+<script>
+export default {
+  /* 1) Fetch the data for a single post. I do it
+  the same way I did it in the main page or in the 
+  posts page. But becasue this time what I want is
+  a single post, and a sigle post also has info as
+  author, context and date, these info is incuded 
+  in the object below. */
+
+  asyncData(context, callback) {
+    setTimeout(() => {
+      callback(null, {
+        /* 2) I will use the context object to access the id
+        of this single post (context.route.params.id). For
+        now, I will have the id to be part of the tile. But
+        in a real app, I will use the id to fetch the 
+        idividual post I want. */
+        loadedPost: {
+          id: "1",
+          title: "First Post (ID: " + context.route.params.id + ")",
+          previewText: "This is our first post!",
+          author: 'Maximilian',
+          updatedDate: new Date(),
+          content: 'Some dummy text which is definitely not the preview text though!',
+          thumbnail:
+            "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
+        }
+      });
+    }, 1000);
+  }
+};
+</script>
 
 <style scoped>
 .single-post-page {
