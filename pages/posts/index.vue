62288d5d75a0443ab942f3a40e698ce69bd7f364
Jimena Miguez <jimeblau@gmail.com>
2022-09-05 13:25:33 +0200

    Delete comments

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -13,18 +13,6 @@ export default {
   },
    asyncData(context, callback) {
     setTimeout(() => {
-      /* 1) In order to handle an error, instead of
-      passing null as the firt argument of the
-      callback method, we pass new Error, which is a
-      js built in constructor.  */
-      /* 2) If I go to the post page I will see the
-      error page we created in layouts. This is because
-      nuxt redirects us to that error page if an error
-      ocurrs. */
-      /* 3) It is important to mention that if instead of 
-      using  setTimeout we are using a promise, error
-      will be handled differently. But that we´ll be
-      explained later.*/
       callback(new Error (), {
         loadedPosts: [
           {
